991068155385014f1121315605a251ff1f9674e7
Md. Hozrat Ali Naeem <mdhozratalinaeem@gmail.com>
2026-03-15 01:55:37 +0600

    JS Validation Prac

---
+++ script2.js
@@ -0,0 +1,23 @@
+const unitPrice = 1000;
+
+const quantityInput = document.getElementById("quantity");
+const totalDisplay = document.getElementById("totalPrice");
+
+quantityInput.addEventListener("input", function(){
+
+    let quantity = Number(quantityInput.value);
+
+    if(quantity < 0){
+        alert("Quantity cannot be negative");
+        quantity = 0;
+        quantityInput.value = 0;
+    }
+    let total = unitPrice * quantity;
+
+    totalDisplay.value = total;
+
+    if(total > 1000){
+        alert("Congratulations! You are eligible for a gift coupon.");
+    }
+
+});
