bc7f335e80859f9e33bcde251a5974c63d4a4e62
Md. Hozrat Ali Naeem <mdhozratalinaeem@gmail.com>
2026-03-15 01:37:55 +0600

    JS Validation

--- script1.js
+++ script1.js
@@ -7,19 +7,15 @@ quantityInput.addEventListener("input", function(){
 
     let quantity = Number(quantityInput.value);
 
-    // Validation: Prevent negative quantity
     if(quantity < 0){
         alert("Quantity cannot be negative");
         quantity = 0;
         quantityInput.value = 0;
     }
-
-    // Calculate total
     let total = unitPrice * quantity;
 
     totalDisplay.value = total;
 
-    // Gift coupon condition
     if(total > 1000){
         alert("Congratulations! You are eligible for a gift coupon.");
     }
